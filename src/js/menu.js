24e1d03102a9b2da5871cb349566c1515db408e1
Paco G <Paco@MacBook-Air-de-Paco.local>
2016-05-02 17:47:57 +0200

    first commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,8 @@
-var menu = document.querySelector('.hamburger');
-function toggleMenu(event) {
-this.classList.toggle('is-active');
-document.querySelector('.menuppal').classList.toggle('is_active');
-event.preventDefault();
-}
-menu.addEventListener('click', toggleMenu, false);
+
+$(document).ready(function(){
+	$('.hamburger').click(function() {
+		$('.hamburger').toggleClass('is-active');
+		$('.menuresponsive').toggleClass('is-active');
+		return false;
+	});
+});
---
+++ src/src/js/menu.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('.hamburger');
+function toggleMenu(event) {
+this.classList.toggle('is-active');
+document.querySelector('.menuppal').classList.toggle('is_active');
+event.preventDefault();
+}
+menu.addEventListener('click', toggleMenu, false);
